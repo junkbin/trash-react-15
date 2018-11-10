a0b0acc47a204c235d0edabb9fe8d4a0af93282b
santosh <santosh.ece06@gmail.com>
2018-11-10 11:44:28 +0530

    upadate

--- src/day1/Fourth.js
+++ src/day1/Fourth.js
@@ -6,13 +6,27 @@ class Fourth extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = {"title" : "Koderoom"};
+        const arr = [1, 2, 3, 4, 5];
+        this.state = {"title" : "Koderoom", "dataList": arr};
+    }
+
+
+    componentDidMount() {
+        setInterval(()=>{this.updateDataListState()}, 1000);
+    }
+
+    updateDataListState() {
+
+        let newState = this.state.dataList.map((item)=>{
+            return item + 1;
+        });
+
+        this.setState({"dataList" : newState});
     }
 
 
     render(){
-        const arr = [1, 2, 3, 4, 5];
-        const elementList = arr.map((ielem, index)=>
+        const elementList = this.state.dataList.map((ielem, index)=>
             <div key={index.toString()}>
                 {ielem}
             </div>

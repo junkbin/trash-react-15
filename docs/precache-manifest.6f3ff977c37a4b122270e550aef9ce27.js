3994e064949fb46e3d2b8857b2216f9943831aa1
santosh <santosh.ece06@gmail.com>
2018-11-10 11:44:58 +0530

    live byukd

---
+++ docs/precache-manifest.6f3ff977c37a4b122270e550aef9ce27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c2972c66c64d0aaa334a",
+    "url": "./static/js/main.c2972c66.chunk.js"
+  },
+  {
+    "revision": "1f0767b392968a979a7e",
+    "url": "./static/js/1.1f0767b3.chunk.js"
+  },
+  {
+    "revision": "c2972c66c64d0aaa334a",
+    "url": "./static/css/main.c32ad7e5.chunk.css"
+  },
+  {
+    "revision": "5b9075d72eebbf0f8e4b85d778fc8058",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7eb3bbd9a076b6ac1b45765edae5b54f.js"
+  "./precache-manifest.6f3ff977c37a4b122270e550aef9ce27.js"
 );
 
 workbox.clientsClaim();

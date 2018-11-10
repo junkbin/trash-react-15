36b2b5c3b3a85841ab5f99b9af26d1142c8bebf2
santosh <santosh.ece06@gmail.com>
2018-11-10 08:27:33 +0530

    event

--- src/day1/Third.js
+++ src/day1/Third.js
@@ -7,13 +7,21 @@ class Third extends React.Component {
         super(props);
 
         this.state = {"counter" : 1};
+
+        this.increment = this.increment.bind(this);
+    }
+
+    increment() {
+        console.log(this.state.counter, new Date());
     }
 
     render() {
         return (
             <div className="full-screen make-center textify">
                 <span className="span make-center">{this.state.counter}</span>
-                <button className="btn">CLICK MEE</button>
+                <button className="btn" onClick={this.increment}>CLICK ME 1</button>
+                <button className="btn" onClick={this.increment.bind(this)}>CLICK ME 2</button>
+                <button className="btn" onClick={(e)=> {this.increment(e)}}>CLICK ME 3</button>
             </div>
         );
     }

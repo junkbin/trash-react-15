9995ed06179ddd72396e5d4460300ff7540b924d
santosh <santosh.ece06@gmail.com>
2018-11-11 20:00:57 +0530

    Updates

--- precache-manifest.9b80fcffec2c94719f154b1ea6357c44.js
+++ precache-manifest.9b80fcffec2c94719f154b1ea6357c44.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/trash-react-15/static/js/runtime~main.aff843f8.js"
   },
   {
-    "revision": "39f8225facb8b8bba929",
-    "url": "/trash-react-15/static/js/main.39f8225f.chunk.js"
+    "revision": "aababc3ffae543f77e2a",
+    "url": "/trash-react-15/static/js/main.aababc3f.chunk.js"
   },
   {
     "revision": "47b5672368ef3937be57",
     "url": "/trash-react-15/static/js/1.47b56723.chunk.js"
   },
   {
-    "revision": "39f8225facb8b8bba929",
+    "revision": "aababc3ffae543f77e2a",
     "url": "/trash-react-15/static/css/main.f23937e2.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/trash-react-15/static/css/1.291292fc.chunk.css"
   },
   {
-    "revision": "2ee29abc18e6fc666b6dd082b520bf2a",
+    "revision": "b0441db290aa1b7568649a8617b59088",
     "url": "/trash-react-15/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/trash-react-15/precache-manifest.fc1bd8881d97b0a93642548a57d9ebf4.js"
+  "/trash-react-15/precache-manifest.9b80fcffec2c94719f154b1ea6357c44.js"
 );
 
 workbox.clientsClaim();

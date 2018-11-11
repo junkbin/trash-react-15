0f36e434d3368d0368aea2ff00efacc218f5d09b
santosh <santosh.ece06@gmail.com>
2018-11-11 07:59:00 +0530

    Updates

---
+++ precache-manifest.8aee8bb3352d51671fdf470271bbba2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aff843f873b864f39217",
+    "url": "/trash-react-15/static/js/runtime~main.aff843f8.js"
+  },
+  {
+    "revision": "ed1ff56b552d1b1884d6",
+    "url": "/trash-react-15/static/js/main.ed1ff56b.chunk.js"
+  },
+  {
+    "revision": "41cb6789c4df017ed2e0",
+    "url": "/trash-react-15/static/js/1.41cb6789.chunk.js"
+  },
+  {
+    "revision": "ed1ff56b552d1b1884d6",
+    "url": "/trash-react-15/static/css/main.e4d44f6e.chunk.css"
+  },
+  {
+    "revision": "41cb6789c4df017ed2e0",
+    "url": "/trash-react-15/static/css/1.291292fc.chunk.css"
+  },
+  {
+    "revision": "ff2422d106683b675f93bbfd97c84615",
+    "url": "/trash-react-15/index.html"
+  }
+];
